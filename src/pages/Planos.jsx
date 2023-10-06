3373caa3e394d4ef163d8d984c571fe9f8dfc00b
Carolina Pacífico <84384263+CarolinaPacifico@users.noreply.github.com>
2023-10-06 19:57:40 -0300

    Add files via upload

--- src/pages/Planos.jsx
+++ src/pages/Planos.jsx
@@ -1,11 +1,12 @@
-
-import CardPlano from '../components/card-plano'
+import Footer from '../components/footer';
+import CardPlano from '../components/card-plano';
 
 function Planos() {
 
     return (
       <>
         <CardPlano/>
+        <Footer/>
       </>
     )
   }
