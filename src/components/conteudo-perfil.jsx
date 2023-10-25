ef4df5a3693b0cdcf8b54a5d6b839eca8a5005d3
Carolina Pacífico <84384263+CarolinaPacifico@users.noreply.github.com>
2023-10-25 16:21:44 -0300

    Add files via upload

--- src/components/conteudo-perfil.jsx
+++ src/components/conteudo-perfil.jsx
@@ -6,15 +6,32 @@ function perfil() {
     return (
       
         <div className="perfil">
-        <h1>Perfil</h1>
-        <p>Aqui você pode modificar informações do seu perfil.</p>
-        <img src={fotoperfil} alt="" width="120" height="120" />
-        <label>Nome Completo<input type="text" id="" name="" /></label>
-        <label>Email<input type="email" id="" name="" /></label>
-        <label>Telefone<input type="number" id="" name="" /></label>
-        <label for="story">Experiência</label>
-        <textarea id="story" name="story" rows="5" cols="33"></textarea>
-        <button className="btnperfil" type="button">Publicar Perfil</button>
+            <div>
+                <h1>Perfil</h1>
+                <p>Aqui você pode modificar informações do seu perfil.</p>
+            </div>
+            <div>
+                <img src={fotoperfil} alt="" width="120" height="120" />
+            </div>
+            <div className="perfil-perfis">
+                <div>
+                    <label>Nome Completo</label>
+                    <input type="text" id="" name="" />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" id="" name="" />
+                </div>
+                <div>
+                    <label>Telefone</label>
+                    <input type="number" id="" name="" />
+                </div>
+                <div>
+                    <label for="story">Experiência</label>
+                    <textarea id="story" name="story" rows="5" cols="33"></textarea>
+                </div>
+                <button className="btnperfil" type="button">Salvar</button>
+            </div>
         </div>
       
     )
